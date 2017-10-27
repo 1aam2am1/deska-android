4ad826bb09011d10261f8bfc9cc3b470627b8767
Michal_Marszalek <1aam2am1@gmail.com>
2017-10-28 00:39:07 +0200

    Added Main Layout

--- app/src/main/java/com/a1aam2am1gmail/deska/MainActivity.kt
+++ app/src/main/java/com/a1aam2am1gmail/deska/MainActivity.kt
@@ -1,17 +1,148 @@
 package com.a1aam2am1gmail.deska
 
+import android.annotation.TargetApi
+import android.support.design.widget.TabLayout
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
+import android.view.ViewTreeObserver
+import android.opengl.ETC1.getWidth
+import android.opengl.ETC1.getHeight
+import android.util.Log
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import kotlinx.android.synthetic.main.activity_main.view.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * The [android.support.v4.view.PagerAdapter] that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * [android.support.v4.app.FragmentStatePagerAdapter].
+     */
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        container.adapter = mSectionsPagerAdapter
+
+        container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+
+        tabs.getTabAt(0)?.setIcon(R.mipmap.ic_launcher)
+        tabs.getTabAt(1)?.setIcon(R.mipmap.ic_launcher_round)
+    }
+
+    /**
+     * A [FragmentPagerAdapter] that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            Log.d("Position", position.toString())
+            when (position) {
+                0 -> return PlaceholderFragment.newInstance(position + 1)
+                1 -> return SettingsFragment.newInstance()
+                else -> return PlaceholderFragment.newInstance(position + 1)
+            }
+        }
+
+        override fun getCount(): Int {
+            // Show 3 total pages.
+            return 2
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    class PlaceholderFragment : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+            rootView.textView.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
+
+            rootView.addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
+                override fun onLayoutChange(v: View?, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int) {
+                    if (v != null) {
+                        rootView.slider.layoutParams.width = (rootView.constraintLayout.height * 0.9).toInt()
+
+                        v.removeOnLayoutChangeListener(this)
+                    }
+                }
+            })
+            return rootView
+        }
+
+
+        companion object {
+            /**
+             * The fragment argument representing the section number for this
+             * fragment.
+             */
+            private val ARG_SECTION_NUMBER = "section_number"
+
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            fun newInstance(sectionNumber: Int): PlaceholderFragment {
+                val fragment = PlaceholderFragment()
+                val args = Bundle()
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+                fragment.arguments = args
+                return fragment
+            }
+        }
+    }
+
+    class SettingsFragment : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            val rootView = inflater.inflate(R.layout.fragment_settings, container, false)
+
+            return rootView
+        }
+
+
+        companion object {
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            fun newInstance(): SettingsFragment {
+                return SettingsFragment()
+            }
+        }
     }
 
     /**
